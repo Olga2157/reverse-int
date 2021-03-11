d4674fe64c6ce7e67dc7ad9ab52434cec6d06b23
Olga Golyakova <olgagolyakova@Olgas-MacBook-Air.local>
2021-03-11 23:55:12 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1;
+    }
+    let digitArray = String(n).split("")
+    let reverseDigitArray = digitArray.reverse();
+    let reversedString = reverseDigitArray.join("");
+    return Number(reversedString);
 }
